4ba3d5693763354ea54ec848631a46bc39cd8727
shanmukhsreerangam <86402250+shanmukhsreerangam@users.noreply.github.com>
2022-08-11 16:33:11 +0530

    Adds test to make sure 5 + 5 is equal to 10

--- flask-circleci/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__) #create an instance of the Flask class you just imported
-
-@app.route('/')
-def main():
-    # display a string in the home page
-    return "Welcome to my Flask App"
